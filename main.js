037d9076e30fdd8b1255f437ea25abb45391638b
Replit user <>
2023-12-12 23:49:41 +0000

    Atualização: Quase tudo pronto, mas ainda sem banco de dados - Replit

--- main.js
+++ main.js
@@ -1,13 +1,36 @@
 'use strict'
 
+const modal = document.querySelector('.modal')
+const tbody = document.querySelector('tbody')
+const fornecedor = document.querySelector('#m-fornecedor')
+const equipamento = document.querySelector('#m-equipamento')
+const nSerie = document.querySelector('#m-nSerie')
+const ult_mp = document.querySelector('#m-ult_mp')
+const prox_mp = document.querySelector('#m-prox_mp')
+const certificado = document.querySelector('#m-certificado')
+
+let itens
+let id
+
+const getItensBD = () => JSON.parse(localStorage.getItem('dbFunc')) ?? []
+const setItensBD = (itens) => localStorage.setItem('dbFunc', JSON.stringify(itens))
+
+function loadItens() {
+  itens = getItensBD()
+  tbody.innerHTML = ''
+  itens.forEach((item, index) => {
+    insertItem(item, index)
+  })
+}
+
+loadItens()
+
+
+
 const openModal = () => document.getElementById('modal')
-    .classList.add('active')
+  .classList.add('active')
 
 const closeModal = () => document.getElementById('modal')
-    .classList.remove('active')
+  .classList.remove('active')
 
-document.getElementById('cadastrarEMH')
-    .addEventListener('click', openModal)
 
-document.getElementById('modalClose')
-    .addEventListener('click', closeModal)
