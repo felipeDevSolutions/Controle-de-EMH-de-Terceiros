2bfb898bea804002ae35dfab8f9d90d58f3b31d5
Felipe Costa <felipeejazz@gmail.com>
2023-12-28 18:54:15 -0300

    Autenticação funcionando. Usuários sendo salvos no Firebase

--- script.js
+++ script.js
@@ -1,85 +1,63 @@
-const linkCadastrar = document.querySelectorAll(".form-box .bottom-link a");
-const formPopup = document.querySelector(".form-popup ");
-const loginForm = document.querySelector(".form-box.login form");
-const signupForm = document.querySelector(".form-box.signup form");
-const confirmPasswordError = document.getElementById("confirmPasswordError");
-const emailValidation = document.getElementById("emailValidation");
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+// Função para inicializar o Firebase
+function initFirebase() {
+    // Configuração do Firebase
+    const firebaseConfig = {
+        apiKey: "AIzaSyD7hSB26zKy2ojX1pHHSJCjF6Jqsy0hD1w",
+        authDomain: "controle-de-emhde-terceiros.firebaseapp.com",
+        databaseURL: "https://controle-de-emhde-terceiros-default-rtdb.firebaseio.com",
+        projectId: "controle-de-emhde-terceiros",
+        storageBucket: "controle-de-emhde-terceiros.appspot.com",
+        messagingSenderId: "360717669238",
+        appId: "1:360717669238:web:63e1a635e12054ab19a057"
+    };
 
-// Função para alternar entre o formulário de login e cadastro
-linkCadastrar.forEach(link => {
-    link.addEventListener("click", (e) => {
-        e.preventDefault();
+    // Inicialização do Firebase
+    const app = initializeApp(firebaseConfig);
+    const auth = getAuth();
+    const db = getFirestore();
 
-        // Reseta os valores dos campos do formulário ao mudar entre os formulários
-        loginForm.reset();
-        signupForm.reset();
-        confirmPasswordError.textContent = "";
-        
-        formPopup.classList[link.id === "signup-link" ? 'add' : 'remove']("show-signup");
-    });
-});
+    return { auth, db };
+}
 
 document.addEventListener("DOMContentLoaded", function () {
+    const { auth, db } = initFirebase();
+
+    const linkCadastrar = document.querySelectorAll(".form-box .bottom-link a");
+    const formPopup = document.querySelector(".form-popup ");
     const loginForm = document.querySelector(".form-box.login form");
     const signupForm = document.querySelector(".form-box.signup form");
     const confirmPasswordInput = signupForm.querySelector("#signupConfirmPassword");
     const confirmPasswordError = document.getElementById("confirmPasswordError");
-    const signupEmailInput = signupForm.querySelector("#signupEmail");
+    const emailValidation = document.getElementById("emailValidation");
 
-    // Escutador para o input de email para mostrar o span de validação de email
-    signupEmailInput.addEventListener("input", function (){
-        const email = signupForm.querySelector("#signupEmail").value;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-        if (!emailRegex.test(email)) {
-            console.error("E-mail inválido.");
-            emailValidation.textContent = "Digite um e-mail válido."; // Exibe mensagem de erro de e-mail
-            
-            // Se o campo de e-mail estiver vazio, remova a mensagem de erro
-            if (email.trim() === "") {
-            emailValidation.textContent = "";
-            }     
-
-        } else {
-            // Remover a mensagem de erro se o email for válido
-            emailValidation.textContent = "";
-        }
-    });
-
-    // Escutador para o input de confirmação de senha para mostrar o span enquanto digita
-    confirmPasswordInput.addEventListener("input", function () {
-        const password = signupForm.querySelector("#signupSenha").value;
-        const confirmPassword = confirmPasswordInput.value;
-
-        // Verificar se as senhas correspondem
-        if (password !== confirmPassword) {
-            confirmPasswordError.textContent = "As senhas não correspondem.";
-            if (confirmPassword === ""){
-                confirmPasswordError.textContent = "Este campo é obrigatório";
-            }
-        } else {
-            confirmPasswordError.textContent = "";
-        }
-    });
-
+    // Evento disparado quando o formulário de login é enviado
     loginForm.addEventListener("submit", function (e) {
         e.preventDefault();
 
         const email = loginForm.querySelector("input[type='text']").value;
         const password = loginForm.querySelector("input[type='password']").value;
 
-        auth.signInWithEmailAndPassword(email, password)
+        signInWithEmailAndPassword(auth, email, password)
             .then((userCredential) => {
                 console.log("Usuário autenticado:", userCredential.user);
-                // Redirecione ou execute outras ações após o login
+
+                // Redirecionar para index.html após o login bem-sucedido
+                window.location.href = "index.html";
             })
             .catch((error) => {
                 console.error("Erro de autenticação:", error.message);
+                alert("Usuário não cadastrado. Por favor, faça o cadastro primeiro.");
             });
     });
 
-    signupForm.addEventListener("submit", function (e) {
+    // Evento disparado quando o formulário de cadastro é enviado
+    signupForm.addEventListener("submit", async function (e) {
         e.preventDefault();
 
         const companyName = signupForm.querySelector("#signupEmpresa").value;
@@ -97,14 +75,82 @@ document.addEventListener("DOMContentLoaded", function () {
         // Limpar o span de erro se as senhas são iguais
         confirmPasswordError.textContent = "";
 
-        auth.createUserWithEmailAndPassword(email, password)
-            .then((userCredential) => {
-                console.log("Usuário cadastrado:", userCredential.user);
-                console.log("Nome da empresa:", companyName);
-                // Redirecione ou execute outras ações após o cadastro
-            })
-            .catch((error) => {
-                console.error("Erro de cadastro:", error.message);
+        try {
+            // Criar usuário no Firebase Authentication
+            const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+
+            console.log("Usuário cadastrado:", userCredential.user);
+            console.log("Nome da empresa:", companyName);
+
+            // Adicionar informações adicionais ao Firestore na coleção "Controle de EMH de Terceiros"
+            const userDocRef = await addDoc(collection(db, "Controle de EMH de Terceiros", "users", userCredential.user.uid), {
+                fornecedor: companyName, // Nome da empresa como fornecedor
+                usuario: email,          // Email como nome de usuário
+                senha: password,         // Senha
+                userId: userCredential.user.uid,
             });
+
+            console.log("Documento do usuário adicionado com ID:", userDocRef.id);
+
+            // Redirecione ou execute outras ações após o cadastro
+            window.location.href = "index.html";
+        } catch (error) {
+            if (error.code === 'auth/email-already-in-use') {
+                console.error("Erro de cadastro:", "O e-mail já está em uso. Por favor, escolha outro e-mail.");
+                // Exiba uma mensagem para o usuário informando que o e-mail já está em uso
+            } else {
+                console.error("Erro de cadastro:", error.message);
+            }
+        }
+    });
+
+    // Evento disparado quando o input de email do formulário de cadastro é alterado
+    const signupEmailInput = signupForm.querySelector("#signupEmail");
+    signupEmailInput.addEventListener("input", function () {
+        const email = signupForm.querySelector("#signupEmail").value;
+
+        if (!emailRegex.test(email)) {
+            console.error("E-mail inválido.");
+            emailValidation.textContent = "Digite um e-mail válido."; // Exibe mensagem de erro de e-mail
+
+            // Se o campo de e-mail estiver vazio, remova a mensagem de erro
+            if (email.trim() === "") {
+                emailValidation.textContent = "";
+            }
+
+        } else {
+            // Remover a mensagem de erro se o email for válido
+            emailValidation.textContent = "";
+        }
+    });
+
+    // Evento disparado quando o input de confirmação de senha do formulário de cadastro é alterado
+    confirmPasswordInput.addEventListener("input", function () {
+        const password = signupForm.querySelector("#signupSenha").value;
+        const confirmPassword = confirmPasswordInput.value;
+
+        // Verificar se as senhas correspondem
+        if (password !== confirmPassword) {
+            confirmPasswordError.textContent = "As senhas não correspondem.";
+            if (confirmPassword === "") {
+                confirmPasswordError.textContent = "Este campo é obrigatório";
+            }
+        } else {
+            confirmPasswordError.textContent = "";
+        }
+    });
+
+    // Evento disparado quando os links de login/cadastro são clicados
+    linkCadastrar.forEach(link => {
+        link.addEventListener("click", (e) => {
+            e.preventDefault();
+
+            // Reseta os valores dos campos do formulário ao mudar entre os formulários
+            loginForm.reset();
+            signupForm.reset();
+            confirmPasswordError.textContent = "";
+
+            formPopup.classList[link.id === "signup-link" ? 'add' : 'remove']("show-signup");
+        });
     });
 });
